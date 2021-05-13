6599663495a38d23d48dee29fa1a820e6ca03a3a
Shubham Tyagi <oyo@Shubhams-Mackbook-Pro.local>
2021-05-13 14:00:21 +0530

    insert router in project for menu items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-import HomePage from './pages/homepage/homepage.component'
+import { Route } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/hats' component={HatsPage}/>
     </div>
   );
 }
